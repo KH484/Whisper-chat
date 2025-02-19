5051821794aaefbc127aed2489a065239ee2e69f
Katherine Heard <katherinebeheard@gmail.com>
2025-02-19 16:20:48 +0000

    setup complete ssl and boost along with main menu options

--- whisper.cpp
+++ whisper.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <string>
-// #include <boost/asio.hpp>
-// #include <boost/asio/ssl.hpp>
+// #include "whisperFunctions.h"
+#include <boost/asio.hpp>
+#include <boost/asio/ssl.hpp>
 
 using std::cout;
 using std::cin;
 using std::string;
+
 // namespace asio = boost::asio;
 // namespace ssl = boost::asio::ssl;
 // using boost::asio::ip::tcp;
-
 
 // server sets up the port and listens
 // client connects to the port and receives
@@ -18,10 +19,21 @@ using std::string;
 int main()
 {
   string user1;
-  cout << "Welcome to Whisper Chat";
+  cout << "Whisper Chat" << '\n';
   cout << "What is your name?: ";
   cin >> user1;
-  cout << user1;
+  cout << "Hello " << user1 << "Where busy parents can chat" << '\n';
+
+  int userChoice;
+  cout << "Would you like to:" << '\n';
+  cout << "1: Send a message " << '\n';
+  cout << "2: Wait to receive a message" << '\n';
+  cout << "Please enter your choice of 1 or 2: " << '\n';
+  cin >> userChoice;
+  cout << userChoice;
+
+  //if userChoice is 1 run function 1
+  // if userChoice is 2 run function 2
 
   return 0;
 }
---
+++ whisperFunctions.cpp
@@ -0,0 +1,3 @@
+// function to send
+
+// function to receive
